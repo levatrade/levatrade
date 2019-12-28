e39d1c8118a0c9b27641378b6d91094647e7399e
Michael Williams <mwilliams@flusharcade.com>
2019-12-28 11:10:21 -0800

    First commit.

---
+++ Android/app/src/main/java/levatrade/levatrade/com/levatrade/MainActivity.kt
@@ -0,0 +1,12 @@
+package levatrade.levatrade.com.levatrade
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ iOS/levatrade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  levatrade
+//
+//  Created by Michael Williams on 28/12/19.
+//  Copyright © 2019 Michael Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
